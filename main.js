b09b00e2cc5a088b12ab4f1c948e33a42a557bac
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2019-08-09 11:12:04 +0200

    Add active/inactive menu function and separate files into individual folders

--- main.js
+++ main.js
@@ -1,8 +1,52 @@
-const openMenu = () => {
-    if(!document.querySelectorAll('.menu-burger__bar').forEach(bar => bar.classList.contains('menu-burger__bar--active'))){
-        document.querySelectorAll('.menu-burger__bar').forEach(bar => bar.classList.toggle('menu-burger__bar--active'));
-    }
-    document.querySelector('.menu-mobile').classList.toggle('menu-mobile--active');
-}
+let isThrottled = false;
 
-document.querySelector('.menu-burger').addEventListener('click', openMenu)
+const transformBars = () => {
+  if (
+    !document
+      .querySelectorAll(".menu-burger__bar")
+      .forEach(bar => bar.classList.contains("menu-burger__bar--active"))
+  ) {
+    document
+      .querySelectorAll(".menu-burger__bar")
+      .forEach(bar => bar.classList.toggle("menu-burger__bar--active"));
+  }
+};
+
+const activeMenu = () => {
+  document.body.classList.add("body-hidden");
+  document
+    .querySelector(".menu-mobile")
+    .classList.add("menu-mobile--active-display");
+  setTimeout(() => {
+    document
+      .querySelector(".menu-mobile")
+      .classList.add("menu-mobile--active-opacity");
+  }, 100);
+};
+
+const inActiveMenu = () => {
+  document.body.classList.remove("body-hidden");
+  document
+    .querySelector(".menu-mobile")
+    .classList.remove("menu-mobile--active-opacity");
+  setTimeout(() => {
+    document
+      .querySelector(".menu-mobile")
+      .classList.remove("menu-mobile--active-display");
+  }, 200);
+};
+
+const openMenu = () => {
+  if (isThrottled) return;
+  isThrottled = true;
+  setTimeout(() => (isThrottled = false), 500);
+
+  transformBars();
+  if (!document.body.classList.contains("body-hidden")) {
+    activeMenu();
+  } else {
+    inActiveMenu();
+  }
+};
+
+document.querySelector(".menu-burger").addEventListener("click", openMenu);
